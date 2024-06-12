4c1a536701ac8f9ad0af2ddc21f580d3974e8e03
crystal-patronus <patronus@gmail.com>
2024-06-12 09:22:45 -0700

    nextjs-dashboard: added invoice page & search & pagenation

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,3 @@
-import { Card } from '@/app/ui/dashboard/cards';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
