e9461ed4a1a5fef1198c55b2ea9884b3dd79188c
crystal-patronus <patronus@gmail.com>
2024-06-13 10:46:36 -0700

    nextjs-dashboard: add authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,27 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentails', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+}
 
 const FormSchema = z.object({
     id: z.string(),
